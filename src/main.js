2df8787b4d988c7014a397868e4246ecd6587410
eldertek <andrelauret@eclipse-technology.eu>
2023-08-15 19:45:04 +0200

    convert into vue templating

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { generateFilePath } from '@nextcloud/router'
+
+import Vue from 'vue'
+import App from './App'
+
+// eslint-disable-next-line
+__webpack_public_path__ = generateFilePath(appName, '', 'js/')
+
+Vue.mixin({ methods: { t, n } })
+
+export default new Vue({
+	el: '#content',
+	render: h => h(App),
+})
