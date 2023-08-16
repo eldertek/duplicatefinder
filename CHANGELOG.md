4fc26daf704cb9fa1c5d91eb17caaf82995f9d29
eldertek <andrelauret@eclipse-technology.eu>
2023-08-16 10:27:54 +0200

    use @nextcoud/vue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # 1.0.1
+- Added new preview in appstore
 - Added new user interface and administration section.
-- Removed ignored file management from the web interface (may be added again in the future).
 # 1.0.0
 Added the first version of the app which is able to find duplicates. Working on NC26, NC27, NC28.
--- src/Settings.vue
+++ src/Settings.vue
@@ -1,59 +1,83 @@
 <template>
-    <NcAppSettingsSection :title="gettext('Duplicate Finder')">
-        <NcCheckboxRadioSwitch :label="gettext('Ignore Mounted Files')" :value="settings.ignore_mounted_files"
-            @input="updateSetting('ignore_mounted_files', $event)">
-            <template #hint>{ gettext('When true, files mounted on external storage will be ignored...') }</template>
-        </NcCheckboxRadioSwitch>
-
-        <NcCheckboxRadioSwitch :label="gettext('Disable event-based detection')" :value="settings.disable_filesystem_events"
-            @input="updateSetting('disable_filesystem_events', $event)">
-            <template #hint>{ gettext('When true, the event-based detection will be disabled...') }</template>
-        </NcCheckboxRadioSwitch>
-
-        <NcInputField :label="gettext('Background Job Interval (Cleanup)')"
-            :value="settings.backgroundjob_interval_cleanup" @input="updateSetting('backgroundjob_interval_cleanup', $event)">
-            <template #hint>{ gettext('Interval in seconds in which the clean-up background job will be run...') }</template>
-        </NcInputField>
-
-        <NcInputField :label="gettext('Background Job Interval (Find Duplicates)')"
-            :value="settings.backgroundjob_interval_find" @input="updateSetting('backgroundjob_interval_find', $event)">
-            <template #hint>{ gettext('Interval in seconds in which the background job, to find duplicates, will be run...') }</template>
-        </NcInputField>
-    </NcAppSettingsSection>
-</template>
+    <div>
+      <h2>Duplicate Finder - Settings</h2>
   
-<script>
-import { NcCheckboxRadioSwitch, NcAppSettingsSection, NcInputField } from '@nextcloud/vue'
-import { generateUrl } from '@nextcloud/router'
-import axios from '@nextcloud/axios'
-
-export default {
-    name: 'Settings',
+      <NcActions>
+        <NcActionCheckbox
+          :checked="settings.ignore_mounted_files"
+          @change="settings.ignore_mounted_files = $event"
+          id="ignore_mounted_files">
+          Ignore Mounted Files
+        </NcActionCheckbox>
+        <p>When true, files mounted on external storage will be ignored...</p>
+  
+        <NcActionCheckbox
+          :checked="settings.disable_filesystem_events"
+          @change="settings.disable_filesystem_events = $event"
+          id="disable_filesystem_events">
+          Disable Filesystem Events
+        </NcActionCheckbox>
+        <p>When true, the event-based detection will be disabled.</p>
+  
+        <NcActionInput
+          type="number"
+          v-model="settings.backgroundjob_interval_cleanup"
+          label="Background Job Cleanup Interval (seconds)"
+          id="backgroundjob_interval_cleanup">
+        </NcActionInput>
+  
+        <NcActionInput
+          type="number"
+          v-model="settings.backgroundjob_interval_find"
+          label="Background Job Find Duplicates Interval (seconds)"
+          id="backgroundjob_interval_find">
+        </NcActionInput>
+  
+        <NcActionTextEditable
+          v-model="settings.ignored_files"
+          label="Ignored Files (JSON Array)"
+          id="ignored_files"
+          placeholder='Example: [[{"attribute":"filename","operator":"=","value":"1_1.txt"}]]'>
+        </NcActionTextEditable>
+      </NcActions>
+  
+      <NcActionButton @click="saveSettings" icon="icon-confirm">Save Settings</NcActionButton>
+    </div>
+  </template>
+  
+  <script>
+  import {
+    NcActions,
+    NcActionCheckbox,
+    NcActionInput,
+    NcActionTextEditable,
+    NcActionButton
+  } from '@nextcloud/vue'
+  
+  export default {
     components: {
-        NcCheckboxRadioSwitch,
-        NcAppSettingsSection,
-        NcInputField,
+      NcActions,
+      NcActionCheckbox,
+      NcActionInput,
+      NcActionTextEditable,
+      NcActionButton
     },
     data() {
-        return {
-            settings: {}
-        };
-    },
-    async mounted() {
-        try {
-            const response = await axios.get(generateUrl('/apps/duplicatefinder/api/v1/settings'));
-            this.settings = response.data.data;
-        } catch (error) {
-            console.error(error);
+      return {
+        settings: {
+          ignore_mounted_files: false,
+          disable_filesystem_events: false,
+          backgroundjob_interval_cleanup: 432000,
+          backgroundjob_interval_find: 172800,
+          ignored_files: "[]"
         }
+      }
     },
     methods: {
-        gettext(message) {
-            return message;
-        },
-    },
-};
-</script>
-  
-<style scoped></style>
+      saveSettings() {
+        // Logic to save the settings data
+      }
+    }
+  }
+  </script>
   
