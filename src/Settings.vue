23623acb0b552cf2d6ddea46e79eafde5d1cf7a2
eldertek <andrelauret@eclipse-technology.eu>
2023-08-16 20:12:56 +0200

    change event name

--- src/Settings.vue
+++ src/Settings.vue
@@ -1,29 +1,39 @@
 <template>
   <div id="duplicatefinder_settings_form">
     <NcSettingsSection :name="t('duplicatefinder', 'Duplicate Finder Settings')"
-      :description="t('duplicatefinder', 'All general settings to modify Duplicate Finder behaviors.')" :limit-width="true">
+      :description="t('duplicatefinder', 'All general settings to modify Duplicate Finder behaviors.')"
+      :limit-width="true">
     </NcSettingsSection>
 
     <NcSettingsSection :name="t('duplicatefinder', 'Ignore Mounted Files')"
-      :description="t('duplicatefinder', 'When true, files mounted on external storage will be ignored.')" :limit-width="true">
+      :description="t('duplicatefinder', 'When true, files mounted on external storage will be ignored.')"
+      :limit-width="true">
       <NcCheckboxRadioSwitch :checked.sync="settings.ignore_mounted_files"
-      @change="saveSettings('ignore_mounted_files', settings.ignore_mounted_files)">{{ t('duplicatefinder','Ignore mounted file') }}</NcCheckboxRadioSwitch>
+        @update:checked="saveSettings('ignore_mounted_files', settings.ignore_mounted_files)">
+        {{ t('duplicatefinder', 'Ignore mounted file') }}
+      </NcCheckboxRadioSwitch>
     </NcSettingsSection>
 
     <NcSettingsSection :name="t('duplicatefinder', 'Disable Filesystem Events')"
       :description="t('duplicatefinder', 'When true, the event-based detection will be disabled.')" :limit-width="true">
-      <NcCheckboxRadioSwitch :checked.sync="settings.disable_filesystem_events" @change="saveSettings('disable_filesystem_events', settings.disable_filesystem_events)">{{ t('duplicatefinder','Disable filesystem events') }}</NcCheckboxRadioSwitch>
+      <NcCheckboxRadioSwitch :checked.sync="settings.disable_filesystem_events"
+        @update:checked="saveSettings('disable_filesystem_events', settings.disable_filesystem_events)">
+        {{ t('duplicatefinder', 'Disable filesystem events') }}
+      </NcCheckboxRadioSwitch>
     </NcSettingsSection>
 
     <NcSettingsSection :name="t('duplicatefinder', 'Background Job Cleanup Interval (seconds)')"
       :description="t('duplicatefinder', 'The interval in seconds for the cleanup background job.')" :limit-width="true">
-      <NcTextField :value.sync="settings.backgroundjob_interval_cleanup" @change="saveSettings('backgroundjob_interval_cleanup', settings.backgroundjob_interval_cleanup)"></NcTextField>
+      <NcTextField :value.sync="settings.backgroundjob_interval_cleanup"
+        @update:value="saveSettings('backgroundjob_interval_cleanup', settings.backgroundjob_interval_cleanup)">
+      </NcTextField>
     </NcSettingsSection>
 
     <NcSettingsSection :name="t('duplicatefinder', 'Background Job Find Duplicates Interval (seconds)')"
       :description="t('duplicatefinder', 'The interval in seconds for the find duplicates background job.')"
       :limit-width="true">
-      <NcTextField :value.sync="settings.backgroundjob_interval_find" @change="saveSettings('backgroundjob_interval_find', settings.backgroundjob_interval_find)"></NcTextField>
+      <NcTextField :value.sync="settings.backgroundjob_interval_find"
+        @update:value="saveSettings('backgroundjob_interval_find', settings.backgroundjob_interval_find)"></NcTextField>
     </NcSettingsSection>
   </div>
 </template>
