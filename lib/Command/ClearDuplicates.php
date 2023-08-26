11a4ed0ceed5489911692423759d2f9525ff293b
André Théo LAURET <andrelauret@eclipse-technology.eu>
2023-08-26 22:13:13 +0000

    clear duplicate with symfony

--- lib/Command/ClearDuplicates.php
+++ lib/Command/ClearDuplicates.php
@@ -12,19 +12,15 @@ use OCA\DuplicateFinder\Service\FileDuplicateService;
 
 class ClearDuplicates extends Command
 {
-    /** @var FileInfoService */
     protected $fileInfoService;
-
-    /** @var FileDuplicateService */
     protected $fileDuplicateService;
 
-    public function __construct(
-        FileInfoService $fileInfoService,
-        FileDuplicateService $fileDuplicateService
-    ) {
+    public function __construct(FileInfoService $fileInfoService, FileDuplicateService $fileDuplicateService)
+    {
+        parent::__construct();
+
         $this->fileInfoService = $fileInfoService;
         $this->fileDuplicateService = $fileDuplicateService;
-        parent::__construct();
     }
 
     protected function configure(): void
@@ -32,34 +28,27 @@ class ClearDuplicates extends Command
         $this
             ->setName('duplicates:clear')
             ->setDescription('Clear all duplicates and information for discovery')
-            ->setHelp(
-                'Remove links to interactively recognized duplicate files from the database of your Nextcloud instance.'
-                . "\n" . 'This action doesn\'t remove the files from your file system.'
-            )
-            ->addOption('force', 'f', InputOption::VALUE_NONE, 'don\'t ask any questions');
+            ->setHelp('Remove links to interactively recognized duplicate files from the database of your Nextcloud instance.'
+                . PHP_EOL . 'This action doesn\'t remove the files from your file system.')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Don\'t ask any questions');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $isClearingRequested = $input->getOption('force');
-        if ($isClearingRequested !== true) {
-            $helper = $this->getHelper('question');
-            $question = new ConfirmationQuestion(
-                'Do you really want to clear all duplicates and information for discovery?',
-                false
-            );
-            $isClearingRequested = $helper->ask($input, $output, $question);
-            if ($isClearingRequested === false) {
-                return 0;
-            }
-        }
-
-        if ($isClearingRequested === true) {
+        if ($input->getOption('force') || $this->confirmClearing($input, $output)) {
             $this->fileDuplicateService->clear();
             $this->fileInfoService->clear();
-            return 0;
+            return Command::SUCCESS;
         }
 
-        return 1;
+        return Command::FAILURE;
+    }
+
+    private function confirmClearing(InputInterface $input, OutputInterface $output): bool
+    {
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('Do you really want to clear all duplicates and information for discovery?', false);
+        return $helper->ask($input, $output, $question);
     }
 }
