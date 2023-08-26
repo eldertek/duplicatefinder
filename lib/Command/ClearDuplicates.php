e7508dd6d492bdd5c34317ee55a65164f4d5e535
André Théo LAURET <andrelauret@eclipse-technology.eu>
2023-08-26 21:36:11 +0000

    use symfony for command

--- lib/Command/ClearDuplicates.php
+++ lib/Command/ClearDuplicates.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace OCA\DuplicateFinder\Command;
 
-use OC\Core\Command\Base;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,9 +10,8 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
 use OCA\DuplicateFinder\Service\FileInfoService;
 use OCA\DuplicateFinder\Service\FileDuplicateService;
 
-class ClearDuplicates extends Base
+class ClearDuplicates extends Command
 {
-
     /** @var FileInfoService */
     protected $fileInfoService;
 
@@ -22,23 +22,21 @@ class ClearDuplicates extends Base
         FileInfoService $fileInfoService,
         FileDuplicateService $fileDuplicateService
     ) {
-        parent::__construct();
         $this->fileInfoService = $fileInfoService;
         $this->fileDuplicateService = $fileDuplicateService;
+        parent::__construct();
     }
 
-    protected function configure():void
+    protected function configure(): void
     {
         $this
             ->setName('duplicates:clear')
             ->setDescription('Clear all duplicates and information for discovery')
             ->setHelp(
                 'Remove links to interactively recognized duplicate files from the database of your Nextcloud instance.'
-                ."\n".'This action doesn\'t remove the files from your file system.'
+                . "\n" . 'This action doesn\'t remove the files from your file system.'
             )
             ->addOption('force', 'f', InputOption::VALUE_NONE, 'don\'t ask any questions');
-
-        parent::configure();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -47,7 +45,7 @@ class ClearDuplicates extends Base
         if ($isClearingRequested !== true) {
             $helper = $this->getHelper('question');
             $question = new ConfirmationQuestion(
-                'Do you realy want to clear all duplicates and information for discovery?',
+                'Do you really want to clear all duplicates and information for discovery?',
                 false
             );
             $isClearingRequested = $helper->ask($input, $output, $question);
