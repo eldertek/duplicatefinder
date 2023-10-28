cd48196b18ab2f89fbec9a64642020093dcaf62d
eldertek <andrelauret@eclipse-technology.eu>
2023-10-28 11:55:27 +0200

    add migration script

---
+++ lib/Migration/Version0007Date20231028094720.php
@@ -0,0 +1,37 @@
+<?php
+namespace OCA\DuplicateFinder\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Migration step for adding "acknowledged" column.
+ */
+class Version1000Date20231028094720 extends SimpleMigrationStep {
+
+    /**
+     * @param IOutput $output
+     * @param Closure(): ISchemaWrapper $schemaClosure
+     * @param array $options
+     * @return null|ISchemaWrapper
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        if ($schema->hasTable('duplicatefinder_dups')) {
+            $table = $schema->getTable('duplicatefinder_dups');
+            
+            if (!$table->hasColumn('acknowledged')) {
+                $table->addColumn('acknowledged', 'boolean', [
+                    'notnull' => true,
+                    'default' => false
+                ]);
+            }
+        }
+
+        return $schema;
+    }
+}
