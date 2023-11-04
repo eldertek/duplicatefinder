c5581488c4912c7c2e9150d522a83a029e0c8fa9
eldertek <andrelauret@eclipse-technology.eu>
2023-11-04 12:01:21 +0100

    fix wrong import on timed job

--- lib/BackgroundJob/ClearDuplicates.php
+++ lib/BackgroundJob/ClearDuplicates.php
@@ -8,7 +8,7 @@ namespace OCA\DuplicateFinder\BackgroundJob;
  use OCP\IDBConnection;
  use OCP\IUser;
  use OCP\IUserManager;
- use OCP\BackgroundJob\TimedJob;
+ use OC\BackgroundJob\TimedJob;
  use Psr\Log\LoggerInterface;
  
  class ClearDuplicates extends TimedJob
